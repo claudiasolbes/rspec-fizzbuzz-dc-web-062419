6c48f0d53918cba2345158c2510a7f57ea0cc3e8
Claudia Solbes <cssolbes@hotmail.com>
2019-06-06 03:31:40 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -8,7 +8,7 @@ end
 def fizzbuzz(int)
   if (int % 3 == 0) && (int % 5 == 0) # if the number int is divisible by 3 & 5
     "FizzBuzz"
-  else int % 3 == 0 # if the number int is divisible by 3
+  elsif int % 3 == 0 # if the number int is divisible by 3
     "Fizz"
   else int % 5 == 0 # if the number int is divisible by 5
     "Buzz"
